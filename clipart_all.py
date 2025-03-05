6088773f9ababe5ad24612d573fb698fcf431724
devon <devonjhills@gmail.com>
2025-03-05 00:35:42 -0500

    update clipart gen

--- clipart_all.py
+++ clipart_all.py
@@ -1,19 +1,24 @@
 import subprocess
 import sys
+import argparse
 
-# Check for an optional max_size parameter passed to clipart_all.py
-max_size = None
-if len(sys.argv) > 1:
-    try:
-        max_size = int(sys.argv[1])
-    except ValueError:
-        pass
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Run clipart scripts.')
+parser.add_argument('-s', action='store_true', help='Run strokes.py instead of clipart.py')
+parser.add_argument('-m', '--max_size', type=int, default=None, help='Maximum size for clipart_resize.py (optional)')
+
+# Parse the arguments
+args = parser.parse_args()
 
 # Build the command for clipart_resize.py with an optional --max_size argument.
 clipart_resize_cmd = ['python', 'clipart_resize.py']
-if max_size is not None:
-    clipart_resize_cmd.extend(['--max_size', str(max_size)])
+if args.max_size is not None:
+    clipart_resize_cmd.extend(['--max_size', str(args.max_size)])
 subprocess.run(clipart_resize_cmd)
 
-subprocess.run(['python', 'clipart.py'])
+if args.s:
+    subprocess.run(['python', 'strokes.py'])
+else:
+    subprocess.run(['python', 'clipart.py'])
+
 subprocess.run(['python', 'zip.py'])
